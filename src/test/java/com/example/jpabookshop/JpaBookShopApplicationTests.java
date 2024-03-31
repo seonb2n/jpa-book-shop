49cda14ebcb4b9f4fb2d4a0397a17101507da8a9
seonb2n <626ksb@gmail.com>
2024-03-31 11:25:56 +0900

    - init pjt

---
+++ src/test/java/com/example/jpabookshop/JpaBookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpabookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBookShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
